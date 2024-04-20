11f95e9af9b48f061d29fd773c1f9a729e8b85b8
Zane Zhu <darciegarick@gmail.com>
2024-04-20 22:16:12 +0800

    新增Error错误码

--- global/error.go
+++ global/error.go
@@ -6,13 +6,31 @@ type CustomError struct {
 }
 
 type CustomErrors struct {
-	BusinessError CustomError
-	ValidateError CustomError
-	TokenError    CustomError
+	BusinessError       CustomError
+	ValidateError       CustomError
+	TokenError          CustomError
+	AnswerError         CustomError
+	CompileError        CustomError
+	MemoryLimitExceeded CustomError
+	TimeLimitExceeded   CustomError
+	PresentationError   CustomError
+	OutputLimitExceeded CustomError
+	DangerousOperation  CustomError
+	RuntimeError        CustomError
+	SystemError         CustomError
 }
 
 var Errors = CustomErrors{
-	BusinessError: CustomError{40000, "业务错误"},
-	ValidateError: CustomError{42200, "请求参数错误"},
-	TokenError:    CustomError{40100, "登录授权失效"},
+	BusinessError:       CustomError{40000, "业务错误"},
+	ValidateError:       CustomError{42200, "请求参数错误"},
+	TokenError:          CustomError{40100, "登录授权失效"},
+	AnswerError:         CustomError{50001, "答案错误"},
+	CompileError:        CustomError{50002, "编译错误"},
+	MemoryLimitExceeded: CustomError{50003, "内存溢出"},
+	TimeLimitExceeded:   CustomError{50004, "超时"},
+	PresentationError:   CustomError{50005, "格式错误"},
+	OutputLimitExceeded: CustomError{50006, "输出溢出"},
+	DangerousOperation:  CustomError{50007, "危险操作"},
+	RuntimeError:        CustomError{50008, "用户程序运行错误"},
+	SystemError:         CustomError{50009, "系统错误"},
 }
--- routes/api.go
+++ routes/api.go
@@ -2,8 +2,6 @@ package routes
 
 import (
 	"jassue-gin/controller/app"
-	"jassue-gin/middleware"
-	"jassue-gin/service"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -17,10 +15,14 @@ func SetApiGroupRoutes(router *gin.RouterGroup) {
 
 	router.POST("/auth/register", app.Register)
 	router.POST("/auth/login", app.Login)
+	router.POST("/auth/info", app.Info)
 
-	authRouter := router.Group("").Use(middleware.JWTAuth(service.AppGuardName))
-	{
-		authRouter.POST("/auth/info", app.Info)
-		authRouter.POST("/auth/logout", app.Logout)
-	}
+	// authRouter := router.Group("").Use(middleware.JWTAuth(service.AppGuardName))
+	// {
+	// 	authRouter.POST("/auth/info", app.Info)
+	// 	authRouter.POST("/auth/logout", app.Logout)
+	// }
+
+	// 示例demo 暂不做权限配置
+
 }
